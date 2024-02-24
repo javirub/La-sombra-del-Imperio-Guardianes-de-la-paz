80296b1a12936868d771365b8bdc43127abb3cc9
javirub <javi__1997@hotmail.com>
2024-02-24 15:05:02 +0100

    Background Scroll

--- main.py
+++ main.py
@@ -2,31 +2,40 @@ import pygame, sys
 from pygame.locals import *
 
 # Initialize the game
-
 pygame.init()
 
-SCREEN = pygame.display.set_mode((720, 1280))
+# Define constants
+WIDTH, HEIGHT = 576, 1024
+FPS = 144
+CLOCK = pygame.time.Clock()
+
+# Set up the window
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# Set up the window title, icon and background
 pygame.display.set_caption('Game')
+ICON = pygame.image.load('Images/icon.png')
+pygame.display.set_icon(ICON)
+BACKGROUND = pygame.image.load('Images/backgrounds/Starfield 1.png').convert()
 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-GREEN = (0, 255, 0)
+# Init y
+y = 0
 
-SCREEN.fill(WHITE)
-
-# Draw a rectangle TODO: put the player sprite here
-player = pygame.draw.rect(SCREEN, RED, (300, 1100, 100, 100))
-
-# Test bullet TODO: put the bullet sprite here, and make it move
-bullet1 = pygame.draw.line(SCREEN, BLACK, (320, 1100), (320, 1000), 5)
-bullet2 = pygame.draw.line(SCREEN, BLACK, (380, 1100), (380, 1000), 5)
-
+# Run the game loop
 while True:
     for event in pygame.event.get():
+
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-    pygame.display.update()
 
+    # Adds background scrolling
+    relative_y = y % BACKGROUND.get_rect().height
+    SCREEN.blit(BACKGROUND, (0, (relative_y - BACKGROUND.get_rect().height)))
+    y += 0.1
+    if relative_y < HEIGHT:
+        SCREEN.blit(BACKGROUND, (0, relative_y))
+
+    # Updates the display and sets the FPS
+    pygame.display.update()
+    CLOCK.tick(FPS)
